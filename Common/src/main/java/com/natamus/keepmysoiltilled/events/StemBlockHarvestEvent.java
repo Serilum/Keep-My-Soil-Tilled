6486d5e6e0c1552aa03d1d307377d087713370c0
Rick South <rick@natamus.com>
2024-06-26 11:41:31 +0200

    (v2.3) Fixed manually placed blocks below crops being turned into farmland even if they're not dirt.

--- Common/src/main/java/com/natamus/keepmysoiltilled/events/StemBlockHarvestEvent.java
+++ Common/src/main/java/com/natamus/keepmysoiltilled/events/StemBlockHarvestEvent.java
@@ -2,6 +2,7 @@ package com.natamus.keepmysoiltilled.events;
 
 import com.natamus.keepmysoiltilled.util.Util;
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -11,26 +12,29 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class StemBlockHarvestEvent {
-	public static void onCropBlockBreak(Level world, Player player, BlockPos pos, BlockState state, BlockEntity blockEntity) {
-		if (world.isClientSide) {
+	public static void onCropBlockBreak(Level level, Player player, BlockPos pos, BlockState state, BlockEntity blockEntity) {
+		if (level.isClientSide) {
 			return;
 		}
 
 		if (Util.isStemGrownBlock(state.getBlock())) {
-			BlockState farmstate = null;
-			boolean foundfarmland = false;
+			BlockState farmBlockState = null;
+			boolean foundFarmBlock = false;
 
 			for (BlockPos np : BlockPos.betweenClosed(pos.getX() - 1, pos.getY() - 1, pos.getZ() - 1, pos.getX() + 1, pos.getY() - 1, pos.getZ() + 1)) {
-				Block nb = world.getBlockState(np).getBlock();
+				Block nb = level.getBlockState(np).getBlock();
 				if (nb instanceof FarmBlock) {
-					foundfarmland = true;
-					farmstate = Blocks.FARMLAND.defaultBlockState();
+					foundFarmBlock = true;
+					farmBlockState = Blocks.FARMLAND.defaultBlockState();
 					break;
 				}
 			}
 
-			if (foundfarmland && farmstate != null) {
-				world.setBlock(pos.below(), farmstate, 3);
+			if (foundFarmBlock && farmBlockState != null) {
+				BlockPos belowPos = pos.below();
+				if (level.getBlockState(belowPos).is(BlockTags.DIRT)) {
+					level.setBlock(belowPos, farmBlockState, 3);
+				}
 			}
 		}
 	}
--- Common/src/main/java/com/natamus/keepmysoiltilled/util/Reference.java
+++ Common/src/main/java/com/natamus/keepmysoiltilled/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.keepmysoiltilled.util;
 public class Reference {
 	public static final String MOD_ID = "keepmysoiltilled";
 	public static final String NAME = "Keep My Soil Tilled";
-	public static final String VERSION = "2.2";
+	public static final String VERSION = "2.3";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
