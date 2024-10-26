2f16b18b881a7b5c737794a69102fd00a305d49a
Rick South <rick@natamus.com>
2024-10-26 15:10:22 +0200

    Keep My Soil Tilled source code for 1.21.3.

---
+++ NeoForge/src/main/java/com/natamus/keepmysoiltilled/neoforge/events/NeoForgeStemBlockHarvestEvent.java
@@ -0,0 +1,21 @@
+package com.natamus.keepmysoiltilled.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.keepmysoiltilled.events.StemBlockHarvestEvent;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeStemBlockHarvestEvent {
+	@SubscribeEvent
+	public static void onCropBlockBreak(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		StemBlockHarvestEvent.onCropBlockBreak(level, e.getPlayer(), e.getPos(), e.getState(), null);
+	}
+}
