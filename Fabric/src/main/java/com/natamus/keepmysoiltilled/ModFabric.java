a591ad4d06cabcfc0e0a9180986bf019bda4f36b
Rick South <rick@natamus.com>
2026-04-10 02:51:33 +0200

    Keep My Soil Tilled source code for 26.1.2.

---
+++ Fabric/src/main/java/com/natamus/keepmysoiltilled/ModFabric.java
@@ -0,0 +1,42 @@
+package com.natamus.keepmysoiltilled;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
+import com.natamus.keepmysoiltilled.events.StemBlockHarvestEvent;
+import com.natamus.keepmysoiltilled.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveBlockEvents.BLOCK_DESTROY.register((Level world, Player player, BlockPos blockPos, BlockState blockState, BlockEntity blockEntity, ItemStack itemStack) -> {
+			StemBlockHarvestEvent.onCropBlockBreak(world, player, blockPos, blockState, blockEntity);
+			return true;
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
